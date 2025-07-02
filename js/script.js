289c0d54794c8cb5465063b7c6e8372d9ade2576
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-02 15:30:39 +0200

    [UPD] inizio multiplo di 3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0){
+for (let i = 1; i <= 100; i++) { // lo uso per alternare da 1 a 100
+  if (i % 3 === 0 && i % 5 === 0){ // Multipli di 2 e 5 
+  console.log("FizzBuzz");
+  
+  } else if (i % 3 === 0){
+    console.log("Fizz");
     
   }
 }
