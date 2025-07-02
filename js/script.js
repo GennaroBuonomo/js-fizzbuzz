5037938b0364bd09e69d0fc76cdeea2b9290057b
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-02 15:33:28 +0200

    [UPD] Inizio i multipli di 5

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ for (let i = 1; i <= 100; i++) { // lo uso per alternare da 1 a 100
   if (i % 3 === 0 && i % 5 === 0){ // Multipli di 2 e 5 
   console.log("FizzBuzz");
   
-  } else if (i % 3 === 0){
+  } else if (i % 3 === 0){ // se è multiplo di 3 stampa fizz
     console.log("Fizz");
-    
+
+  }else if (i % 5 === 0){ // se è multiplo di 5 stampa buzz
+    console.log("Buzz")
   }
 }
