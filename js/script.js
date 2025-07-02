b65bf3576f9a3136e7b13da0fa09fd821115ff51
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-02 16:04:07 +0200

    [AND] fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 for (let i = 1; i <= 100; i++) { // lo uso per alternare da 1 a 100
-  if (i % 3 === 0 && i % 5 === 0){ // Multipli di 2 e 5 
-  console.log("FizzBuzz");
-  
-  } else if (i % 3 === 0){ // se è multiplo di 3 stampa fizz
-    console.log("Fizz");
+if (i % 3 === 0 && i % 5 === 0){ // Multipli di 2 e 5 
+console.log("FizzBuzz");
 
-  }else if (i % 5 === 0){ // se è multiplo di 5 stampa buzz
-    console.log("Buzz")
+} else if (i % 3 === 0){ // se è multiplo di 3 stampa fizz
+  console.log("Fizz");
+
+}else if (i % 5 === 0){ // se è multiplo di 5 stampa buzz
+  console.log("Buzz")
+} else {
+      console.log(i);
   }
 }
